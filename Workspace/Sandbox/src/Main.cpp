aa561923248c7c0bda8938ba2778853307134ab3
EmpyreanArcadius <immaculateempyrean@gmail.com>
2020-06-09 19:12:17 +0530

    Added Functionality For Checking Memory Leaks.. As Of Writing There Are Many Memory Leaks..

--- Workspace/Sandbox/src/Main.cpp
+++ Workspace/Sandbox/src/Main.cpp
@@ -2,9 +2,31 @@
 #include<vector>
 #include<string>
 
+//This New Is Used Only For Debugging Purposes Set The Debugging Flag For The Event System To Use This...
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include "Example1.h"
 
 int main()
 {
+	//Start Initialize Memory Leak Checking Module..
+	int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+	flag |= (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(flag);
+	//Ended Initialize Memory Leak Checking Module..
+
+
+	//Start Call Which Example You Want Here//
 	Example1Main();
+	//Ended Call Which Example You Want Here//
+
+	//Start Print Detected Memory Leaks To File..
+	FILE* pFile;
+	freopen_s(&pFile, "MemoryLeakLog.txt", "w", stdout);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtDumpMemoryLeaks(); fclose(pFile);
+	//Ended Print Detected Memory Leaks To File..
 }
